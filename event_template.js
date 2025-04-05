2c1fe5ea5dac70159d2bde9822e44e81d95d4d4b
AdelaideBaron <baronadelaide@hotmail.com>
2025-04-05 21:49:42 +0100

    adding events

---
+++ event_template.js
@@ -0,0 +1,15 @@
+export const events = [
+  {
+    date: "yyyy-mm-dd",
+    time: "6:30 PM",
+    finish_time: "8:30 PM",
+    title: "",
+    location: "",
+    country: "",
+    cost: "",
+    audience: "",
+    description: "",
+    event_type: "",
+    link: "",
+  },
+];
