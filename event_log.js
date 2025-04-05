565a85be2efb67504592101a1ec29a70eb4c57da
AdelaideBaron <baronadelaide@hotmail.com>
2025-04-05 20:23:53 +0100

    adding type

--- event_log.js
+++ event_log.js
@@ -1,11 +1,10 @@
-// event_log.js
 export const events = [
-    {
-      date: '2025-04-05',
-      time: '11:00 PM',
-      title: 'Page Launch',
-      location: 'Online',
-      description: 'Creation of this website'
-    }
-  ];
-  
+  {
+    date: "2025-04-05",
+    time: "11:00 PM",
+    title: "Page Launch",
+    location: "Online",
+    description: "Creation of this website",
+    event_type: "Occassion",
+  },
+];
